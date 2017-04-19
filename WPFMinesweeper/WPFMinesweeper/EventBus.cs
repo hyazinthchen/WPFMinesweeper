62e8a28f352abf4ebfabeb17ec050c08c58a8ac3
Lena Goldmann <goldmann@queo.local>
2017-04-19 16:17:52 +0200

    Added first implementation of a EventBus

---
+++ WPFMinesweeper/WPFMinesweeper/EventBus.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPFMinesweeper
+{
+    public class EventBus : IEventBus
+    {
+        /// <summary>
+        /// Speichert die Subscriber eines Events
+        /// </summary>
+        private readonly Dictionary<Type, List<Object>> _subscribers = new Dictionary<Type, List<object>>();
+        //Besser als Liste? Typ Delegate?
+
+
+        /// <summary>
+        /// Abonniert einen Eventhandler
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="handler"></param>
+        public void Subscribe<TMessage>(Action<TMessage> handler)
+        {
+            //Wenn es schon eine Liste mit Subscribern für dieses Event gibt nutze diese, sonst lege neue an.
+            if (_subscribers.ContainsKey(typeof(TMessage)))
+            {
+                var handlers = _subscribers[typeof(TMessage)];
+                handlers.Add(handler);
+            }
+            else
+            {
+                var handlers = new List<Object>();
+                handlers.Add(handler);
+                _subscribers[typeof(TMessage)] = handlers;
+            }
+        }
+
+        /// <summary>
+        /// Kündigt das Abonnement eines Eventhandlers
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="handler"></param>
+        public void Unsubscribe<TMessage>(Action<TMessage> handler)
+        {
+            //Wenn ein Subscriber unsubscriben will, muss er auch wirklich subscribed sein. Dann kann er von der Liste dieses Events entfernt werden.
+            if (_subscribers.ContainsKey(typeof(TMessage)))
+            {
+                var handlers = _subscribers[typeof(TMessage)];
+                handlers.Remove(handler);
+
+                if (handlers.Count == 0)
+                {
+                    _subscribers.Remove(typeof(TMessage));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ruft gewünschte Events auf
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="message"></param>
+        public void Publish<TMessage>(TMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPFMinesweeper/WPFMinesweeper/IEventBus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WPFMinesweeper
+{
+    /// <summary>
+    /// Interface für den EventBus
+    /// </summary>
+    public interface IEventBus {
+
+        //TMessage = Typ der Message die verschickt wird
+        void Subscribe<TMessage>(Action<TMessage> handler);
+        void Unsubscribe<TMessage>(Action<TMessage> handler);
+        void Publish<TMessage>(TMessage message);
+    }
+    
+}
